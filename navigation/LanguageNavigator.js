cb246ac8ddb5fdbc08da2db9f790b4758d10172c
Karimov Sirojiddin <herodot.sk@mail.ru>
2020-07-20 15:47:14 +0900

    new

--- navigation/LanguageNavigator.js
+++ navigation/LanguageNavigator.js
@@ -1,44 +1,42 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import LoginScreen from '../screens/LoginScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-import TutorialsScreen from '../screens/TutorialsScreen';
-import WelcomeScreen from '../screens/WelcomeScreen'
-import RegisterScreen from '../screens/RegisterScreen'
+import LoginScreen from "../screens/LoginScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import TutorialsScreen from "../screens/TutorialsScreen";
+import WelcomeScreen from "../screens/WelcomeScreen";
+import RegisterScreen from "../screens/RegisterScreen";
 
-const Stack=createStackNavigator()
-const Tab=createBottomTabNavigator()
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-const LanguageNavigator=()=>{
+const LanguageNavigator = () => {
   return (
-     <NavigationContainer>
-       <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-          name="Login" 
-          component={LoginScreen} 
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
           options={{
-            title:'Home',
-            headerStyle:{
-              backgroundColor:'#f4511e'
+            title: "Home",
+            headerStyle: {
+              backgroundColor: "#f4511e",
             },
-            headerTintColor:'#fff',
-            headerTitleStyle:{
-              fontWeight:'bold'
-            }
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
           }}
-          />
-          <Stack.Screen name="Welcome" component={WelcomeScreen}
-          />
-          <Stack.Screen name="Tutorials" component={TutorialsScreen}/>
-          <Stack.Screen name="Profile" component={ProfileScreen}/>
-          <Stack.Screen name="Register" component={RegisterScreen}/>
-       </Stack.Navigator>
-     </NavigationContainer>
-     
+        />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Tutorials" component={TutorialsScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 export default LanguageNavigator;
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,22 +1,47 @@
-import React from 'react'
-import {View, Text, StyleSheet,Button} from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-function WelcomeScreen({navigation}){
-    return(
+function WelcomeScreen({ navigation }) {
+  return (
+    <>
       <View style={styles.container}>
         <Text>Welcome here</Text>
-        <Button title="Go to Tutorials"
-        onPress={()=>navigation.navigate('Tutorials')}/>
+        <Button
+          title="Go to Tutorials"
+          onPress={() => navigation.navigate("Tutorials")}
+        />
       </View>
-    );
-  }
-  const styles=StyleSheet.create({
-    container: {
-        width:'100%',
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-  })
-  export default WelcomeScreen;
+      <View style={styles.container}>
+        <Text>Welcome here</Text>
+        <Button
+          title="Go to Tutorials"
+          onPress={() => navigation.navigate("Tutorials")}
+        />
+      </View>
+      <View style={styles.container}>
+        <Text>Welcome here</Text>
+        <Button
+          title="Go to Tutorials"
+          onPress={() => navigation.navigate("Tutorials")}
+        />
+      </View>
+      <View style={styles.container}>
+        <Text>Welcome here</Text>
+        <Button
+          title="Go to Tutorials"
+          onPress={() => navigation.navigate("Tutorials")}
+        />
+      </View>
+    </>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+export default WelcomeScreen;
